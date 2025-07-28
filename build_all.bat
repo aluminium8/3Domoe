c58ce029bfe280eb1a175aea9a4d8e3e6465e208
masa hane <haneda.masaya1998@gmail.com>
2025-07-28 13:30:53 +0900

    small command pattern sample completed

---
+++ build_all.bat
@@ -0,0 +1,3 @@
+cmake -S ./ -B build  -DCMAKE_TOOLCHAIN_FILE=C:/Users/haned/vcpkg/scripts/buildsystems/vcpkg.cmake
+REM cmake --build build --config Debug -j
+cmake --build build --config Release -j
